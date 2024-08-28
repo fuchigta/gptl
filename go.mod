7fdd7b7c3ca5ca5385c5654509a5c66f6831a33a
alpha-fuchigta <fuchigta@alpha.co.jp>
2024-08-28 18:06:05 +0900

    対話的な初期化ができるように

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/fuchigta/gptl
 
 go 1.21.4
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/manifoldco/promptui v0.9.0
+	golang.org/x/exp v0.0.0-20240823005443-9b4947da3948
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
+)
--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 	"path/filepath"
 
+	"slices"
+
+	"golang.org/x/exp/maps"
+
+	"github.com/manifoldco/promptui"
 	"gopkg.in/yaml.v2"
 )
 
@@ -20,7 +25,7 @@ type Config struct {
 	Endpoint  string `yaml:"endpoint"`
 	ApiKey    string `yaml:"api_key"`
 	Model     string `yaml:"model"`
-	MaxTokens uint   `yaml:"max_tokens"`
+	MaxTokens uint   `yaml:"max_tokens,omitempty"`
 }
 
 func loadProvider(configPath string, historyDirPath string) (Provider, error) {
@@ -45,6 +50,95 @@ func exitErrBy(f string, args ...interface{}) int {
 	return exitErr
 }
 
+func doInit(configPath string) int {
+	dir := filepath.Dir(configPath)
+	dirInfo, err := os.Stat(dir)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return exitErrBy("%s: error: %s", dir, err.Error())
+		}
+
+		if !dirInfo.IsDir() {
+			return exitErrBy("%s is not directory", dir)
+		}
+
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return exitErrBy("%s: mkdir error: %s", dir, err.Error())
+		}
+	}
+
+	info, err := os.Stat(configPath)
+	if err == nil {
+		if info.IsDir() {
+			return exitErrBy("%s is directory", configPath)
+		}
+
+		return exitOk
+	}
+
+	config := Config{}
+
+	providers := maps.Keys(providerFactories)
+	slices.Sort(providers)
+	providerSelect := promptui.Select{
+		Label:        "provider",
+		Items:        providers,
+		HideSelected: true,
+	}
+
+	_, provider, err := providerSelect.Run()
+	if err != nil {
+		return exitErrBy(err.Error())
+	}
+
+	config.Provider = provider
+
+	endpointPrompt := promptui.Prompt{
+		Label:       "endpoint",
+		HideEntered: true,
+	}
+
+	endpoint, err := endpointPrompt.Run()
+	if err != nil {
+		return exitErrBy(err.Error())
+	}
+
+	config.Endpoint = endpoint
+
+	apiKeyPrompt := promptui.Prompt{
+		Label:       "api_key",
+		HideEntered: true,
+	}
+
+	apiKey, err := apiKeyPrompt.Run()
+	if err != nil {
+		return exitErrBy(err.Error())
+	}
+
+	config.ApiKey = apiKey
+
+	modelPrompt := promptui.Prompt{
+		Label:       "model",
+		HideEntered: true,
+	}
+
+	model, err := modelPrompt.Run()
+	if err != nil {
+		return exitErrBy(err.Error())
+	}
+
+	config.Model = model
+
+	content, _ := yaml.Marshal(config)
+	if err := os.WriteFile(configPath, content, 0600); err != nil {
+		return exitErrBy(err.Error())
+	}
+
+	fmt.Printf("%s created\n", configPath)
+
+	return exitOk
+}
+
 func run() int {
 	home := os.Getenv("HOME")
 	if home == "" {
@@ -57,13 +151,19 @@ func run() int {
 		inputPath      = ""
 		outputPath     = ""
 		history        = ""
+		init           = false
 	)
 	flag.StringVar(&configPath, "C", configPath, "config file path")
 	flag.StringVar(&historyDirPath, "H", historyDirPath, "history directory path")
 	flag.StringVar(&inputPath, "i", inputPath, "input file path")
 	flag.StringVar(&outputPath, "o", outputPath, "output file path")
 	flag.StringVar(&history, "h", history, "history name")
+	flag.BoolVar(&init, "init", init, "init config file")
 	flag.Parse()
+
+	if init {
+		return doInit(configPath)
+	}
 
 	provider, err := loadProvider(configPath, historyDirPath)
 	if err != nil {
